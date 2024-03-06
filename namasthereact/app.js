86ad35ee2c9be79315d6740092ce545d50ab061f
rifazahmed7 <158307114+rifazahmed7@users.noreply.github.com>
2024-03-06 11:40:30 +0530

    j
    p

--- namasthereact/app.js
+++ namasthereact/app.js
@@ -6,7 +6,7 @@ root.render(heading)
 const div= React.createElement("div",{id : "div1"},
 [ React.createElement("div",{id : "div2"},"chil2" ),
 React.createElement("div",{id : "div3"},"chil2")])
-root.render(div)
+root.render("div")
 
 
 console.log(heading);
